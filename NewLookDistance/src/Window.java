f72af253d40ea555055c930972492b473785784d
Mark Hawker Jr <markhawkerap@gmail.com>
2020-05-14 02:12:28 -0400

    Client Updates
    4 ranges
    Mappable
    Color coding

--- NewLookDistance/src/Window.java
+++ NewLookDistance/src/Window.java
@@ -4,21 +4,24 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.net.URL;
 
 public class Window {
 
-    String results;
+    String[] results= new String[4];
     String query;
     String queryEdit;
     String cityState;
+    Double businessRange=25.00;
     Window() {
-
-
 
         webScrape view=new webScrape();
         JFrame f = new JFrame("Distance Calculator");
         try {
-            f.setContentPane(new JLabel(new ImageIcon(ImageIO.read(getClass().getResource("resources/images/logo.png")))));
+            JLabel image= new JLabel(new ImageIcon(ImageIO.read(getClass().getResource("resources/images/logo.png")).getScaledInstance(100,60, Image.SCALE_DEFAULT)));
+
+            image.setBounds(0,0,100,100);
+            f.add(image);
         }
         catch(IOException e)
         {
@@ -28,38 +31,58 @@ public class Window {
         ImageIcon img=new ImageIcon("resources/images/logo.png");
         f.setIconImage(img.getImage());
         f.setResizable(false);
-        f.setAlwaysOnTop(true);
+        //f.setAlwaysOnTop(true);
+
         //submit button
         JButton button = new JButton("Calculate");
-        button.setBounds(150, 265, 100, 40);
-        //enter name label
-        JLabel labelZip = new JLabel();
-        labelZip.setText("Enter Zipcode:");
-        labelZip.setBounds(10, -15, 100, 100);
-
-        JLabel labelCity = new JLabel();
-        labelCity.setText("Enter (City,State):");
-        labelCity.setBounds(200, -15, 100, 100);
+        button.setFocusable(false);
+        button.setBounds(125, 265, 100, 40);
+        //enter location labels
+        JLabel labelLocation = new JLabel();
+        labelLocation.setText("Enter Location:  (City,State or Zip)");
+        labelLocation.setBounds(75, -35, 200, 100);
+        
 
         //empty label which will show event after button clicked
-        JLabel result = new JLabel();
-        result.setFont(new Font("Courier", Font.BOLD,19));
-        result.setBounds(80, 195, 300, 100);
+        JLabel result1 = new JLabel();
+        result1.setFont(new Font("SansSerif", Font.BOLD,19));
+        result1.setBounds(20, 65, 400, 100);
+
+        JLabel result2 = new JLabel();
+        result2.setFont(new Font("SansSerif", Font.BOLD,19));
+        result2.setBounds(20, 100, 400, 100);
+
+        JLabel result3 = new JLabel();
+        result3.setFont(new Font("SansSerif", Font.BOLD,19));
+        result3.setBounds(20, 135, 400, 100);
+
+        JLabel result4 = new JLabel();
+        result4.setFont(new Font("SansSerif", Font.BOLD,19));
+        result4.setBounds(20, 170, 400, 100);
+
+        JLabel cityState=new JLabel();
+        cityState.setFont(new Font("SansSerif", Font.BOLD, 23));
+        cityState.setBounds(110, 20, 350, 100);
+
         //textfield to enter name
-        JTextField textfieldZip = new JTextField();
-        textfieldZip.setBounds(100, 25, 80, 30);
+        JTextField txtFieldLocal = new JTextField();
+        txtFieldLocal.setBounds(110, 25, 120, 30);
 
-
-        JTextField textfieldCity = new JTextField();
-        textfieldCity.setBounds(300, 25, 110, 30);
+        JButton mapButton=new JButton("Map It!");
+        mapButton.setBounds(250,265,75,30);
+        mapButton.setEnabled(false);
+        
         //add to frame
-        f.add(result);
-        f.add(textfieldZip);
-        f.add(labelZip);
-        f.add(textfieldCity);
-        f.add(labelCity);
+        f.add(result1);
+        f.add(result2);
+        f.add(result3);
+        f.add(result4);
+        f.add(cityState);
+        f.add(txtFieldLocal);
+        f.add(labelLocation);
         f.add(button);
-        f.setSize(450,350);
+        f.add(mapButton);
+        f.setSize(350,350);
         f.setLayout(null);
         f.setVisible(true);
         f.getRootPane().setDefaultButton(button);
@@ -69,31 +92,71 @@ public class Window {
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent arg0) {
-                if(!textfieldZip.getText().isEmpty()) {
-                    query = textfieldZip.getText();
-                    cityState=view.getCity(query);
-                    results=view.getDistance(cityState);
-                    result.setText(cityState+" "+results);
-                    textfieldZip.setText("");
+                if(!txtFieldLocal.getText().isEmpty()) {
+                    query = txtFieldLocal.getText();
+                    if (query.matches("[0-9]+")) {
+                        results = view.getByZip(query);
+                        query=view.getCity(query);
+                        queryEdit=query.replaceAll("\\s","");
+                    }
+                    else {
+                        queryEdit=query.replaceAll("\\s","");
+                        results =view.getByCity(queryEdit);
+                    }
+                    result1.setForeground(Color.black);
+                    result1.setText(results[0]);
+                    if(Double.parseDouble(results[0].substring(0,results[0].indexOf(" ")))>businessRange){
+                        result1.setForeground(Color.red);
+                        result1.setText(view.isOverrange(results[0]));
+                    }
+
+                    result2.setForeground(Color.black);
+                    result2.setText(results[1]);
+                    if(Double.parseDouble(results[1].substring(0,results[1].indexOf(" ")))>businessRange) {
+                        result2.setText(view.isOverrange(results[1]));
+                        result2.setForeground(Color.red);
+                    }
+
+                    result3.setForeground(Color.black);
+                    result3.setText(results[2]);
+                    if(Double.parseDouble(results[2].substring(0,results[2].indexOf(" ")))>businessRange) {
+                        result3.setText(view.isOverrange(results[2]));
+                        result3.setForeground(Color.red);
+                    }
+                    result4.setForeground(Color.black);
+                    result4.setText(results[3]);
+                    if(Double.parseDouble(results[3].substring(0,results[3].indexOf(" ")))>businessRange) {
+                        result4.setText(view.isOverrange(results[3]));
+                        result4.setForeground(Color.red);
+                    }
+                    txtFieldLocal.setText("");
+                    query=query.substring(0,1).toUpperCase()+query.substring(1);
+                    query=query.substring(0,query.length()-2)+query.substring(query.length()-2).toUpperCase();
+                    cityState.setText("<html><u>"+query+"</u></html>");
+                    mapButton.setEnabled(true);
                 }
-                else if(!textfieldCity.getText().isEmpty()) {
-                    query = textfieldCity.getText();
-                    queryEdit=query.replaceAll("\\s","");
-                    results=view.getDistance(queryEdit);
-                    result.setText(query+" "+results);
-                    textfieldCity.setText("");
-                }
-                else if (textfieldCity.getText().isEmpty()&& textfieldZip.getText().isEmpty()) {
-                    result.setText("Please enter a location!");
+                else if (txtFieldLocal.getText().isEmpty()) {
+                    result1.setText("Please enter a location!");
                 }
             }
         });
-    }
 
-    public String getAnswer() {
-        return this.results;
-    }
-    public void setResult(String answer){
-        this.results=answer;
+        mapButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+                try {
+                    Desktop.getDesktop().browse(new URL("https://maps.googleapis.com/maps/api/staticmap?size=1000x1000&Center=42.1830225,-71.0475334&zoom=9&maptype=roadmap"+
+                            "&markers=color:blue%7Clabel:H%7C4516+Bedford+St+East+Bridgewater,+MA+02333"+
+                            "&markers=color:blue%7Clabel:%7C4Providence,RI"+
+                            "&markers=color:blue%7Clabel:D%7C4Dedham,MA"+
+                            "&markers=color:blue%7Clabel:P%7C4plymouth,ma"+
+                            "&markers=color:red%7Clabel:H%7C4"+queryEdit+
+                            "&key=AIzaSyApSazA37My_KthkgkqLcwRujUoRvV0J7Q").toURI());
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
--- NewLookDistance/src/webScrape.java
+++ NewLookDistance/src/webScrape.java
@@ -2,60 +2,74 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-
-
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.text.DecimalFormat;
 
 public class webScrape {
     String cityName;
     String stateName;
-    String distance;
-    String businessAddress1="516BedfordStreetEastBridgewater,Ma";
-    String businessAddress2="";
-    String businessAddress3="";
-    String urlLink="";
-    String userInput="";
+    String[] businessAddress = {"516BedfordStreetEastBridgewater,Ma", "Dedham,MA", "Providence,RI", "Plymouth,Ma"};
+    String[] businessLocale = {" from Here", " from Scott", " from Sam", " from Plymouth"};
+    String[] distances = new String[businessAddress.length];
+    String urlLink1 = "";
     BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-
-    public String getCity(String zipcode){
-        urlLink="https://maps.googleapis.com/maps/api/geocode/json?address=="+zipcode+"&key=AIzaSyApSazA37My_KthkgkqLcwRujUoRvV0J7Q";
-        try{
-            //dive into the nested JSON to retrieve long and lat
-            JSONObject json = new JSONObject((readUrl(urlLink)));
+    public String getCity(String zipcode) {
+        urlLink1 = "https://maps.googleapis.com/maps/api/geocode/json?address==" + zipcode + "&key=AIzaSyApSazA37My_KthkgkqLcwRujUoRvV0J7Q";
+        try {
+            //dive into the nested JSON to retrieve City and State
+            JSONObject json = new JSONObject((readUrl(urlLink1)));
             JSONArray result = json.getJSONArray("results");
             JSONObject result1 = result.getJSONObject(0);
             JSONArray components = result1.getJSONArray("address_components");
-            JSONObject city =components.getJSONObject(1);
-            cityName=city.getString("long_name");
-            JSONObject state=components.getJSONObject(3);
-            stateName=state.getString("short_name");
-
-        }
-        catch(JSONException a)
-        {
+            JSONObject city = components.getJSONObject(1);
+            cityName = city.getString("long_name");
+            JSONObject state = components.getJSONObject(3);
+            stateName = state.getString("short_name");
+        } catch (JSONException a) {
             System.out.println("Unable to reach website");
-        }
-        catch(Exception v){
+        } catch (Exception v) {
             System.out.println("Crashed");
         }
-        String combo=cityName+","+stateName;
+        String combo = cityName + ", " + stateName;
         return combo;
     }
-
-    public String getDistance(String searchtool){
-        distance="";
-        urlLink="https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins="+businessAddress1+"&destinations="+searchtool+"&key=AIzaSyApSazA37My_KthkgkqLcwRujUoRvV0J7Q";
+    public String[] getByZip(String zipcode) {
+        try {
+            for (int i = 0; i < businessAddress.length; i++) {
+                urlLink1 = "https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=" + businessAddress[i] + "&destinations=" + zipcode + "&key=AIzaSyApSazA37My_KthkgkqLcwRujUoRvV0J7Q";
+                JSONObject json = new JSONObject((readUrl(urlLink1)));
+                JSONArray result = json.getJSONArray("rows");
+                JSONObject result1 = result.getJSONObject(0);
+                JSONArray result2 = result1.getJSONArray("elements");
+                JSONObject result3 = result2.getJSONObject(0);
+                JSONObject result4 = result3.getJSONObject("distance");
+                distances[i] = result4.getString("text");
+                distances[i] = distances[i] + businessLocale[i];
+            }
+        } catch (JSONException a) {
+            System.out.println("Unable to reach website");
+        } catch (Exception v) {
+            System.out.println("Crashed");
+        }
+        sortRanges(distances);
+        return distances;
+    }
+    public String[] getByCity(String searchtool){
         try{
-            JSONObject json = new JSONObject((readUrl(urlLink)));
-            JSONArray result = json.getJSONArray("rows");
-            JSONObject result1=result.getJSONObject(0);
-            JSONArray result2=result1.getJSONArray("elements");
-            JSONObject result3=result2.getJSONObject(0);
-            JSONObject result4=result3.getJSONObject("distance");
-            distance=result4.getString("text");
+            for (int i=0;i<businessAddress.length;i++) {
+                urlLink1="https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins="+businessAddress[i]+"&destinations="+searchtool+"&key=AIzaSyApSazA37My_KthkgkqLcwRujUoRvV0J7Q";
+                JSONObject json = new JSONObject((readUrl(urlLink1)));
+                JSONArray result = json.getJSONArray("rows");
+                JSONObject result1 = result.getJSONObject(0);
+                JSONArray result2 = result1.getJSONArray("elements");
+                JSONObject result3 = result2.getJSONObject(0);
+                JSONObject result4 = result3.getJSONObject("distance");
+                distances[i] = result4.getString("text");
+                distances[i]=distances[i]+businessLocale[i];
+            }
         }
         catch(JSONException a)
         {
@@ -64,18 +78,56 @@ public class webScrape {
         catch(Exception v){
             System.out.println("Crashed");
         }
-
-        if (distance.equals(""))
-            return "City was not found.";
-        int x=searchtool.length();
-        searchtool=searchtool.substring(0,x-2)+" "+searchtool.substring(x-2,x);
-        //distance= searchtool+" is:" +distance+"les.";
         cityName="";
         stateName="";
-        searchtool="";
-       return distance;
+
+        sortRanges(distances);
+       return distances;
+    }
+    public String isOverrange(String string) {
+        double miles=Double.parseDouble(string.substring(0,string.indexOf(" ")));
+        DecimalFormat round=new DecimalFormat("#.#");
+        if((miles>25.00))
+        {
+            Double rounded = miles - 25.00;
+            string = round.format(rounded) + " miles past "+ string.substring(string.lastIndexOf(" ")+1)+"'s range";
+        }
+        return string;
     }
 
+    private String[] sortRanges(String[] distances){
+
+        //sort array from smallest to largest, unless "here" is less than 25 miles
+        boolean hub=false;
+        if(Double.parseDouble(distances[0].substring(0,distances[0].indexOf(" ")))<25.0) {
+            for (int i=2;i<distances.length;i++)
+            {
+                String key=distances[i];
+                int j=i-1;
+                while((j>0) && (Double.parseDouble(distances[j].substring(0,distances[j].indexOf(" ")))>Double.parseDouble(key.substring(0,key.indexOf(" ")))))
+                {
+                   distances[j+1]=distances[j];
+                    j--;
+                }
+                distances[j+1]=key;
+            }
+        }
+        else
+            for (int i=1;i<distances.length;i++)
+            {
+                String key=distances[i];
+                int j=i-1;
+                while((j>-1) && (Double.parseDouble(distances[j].substring(0,distances[j].indexOf(" ")))>Double.parseDouble(key.substring(0,key.indexOf(" ")))))
+                {
+                    distances[j+1]=distances[j];
+                    j--;
+                }
+                distances[j+1]=key;
+            }
+
+
+        return distances;
+    }
     private static String readUrl(String urlString) throws Exception {
         BufferedReader reader = null;
         try {
@@ -93,4 +145,5 @@ public class webScrape {
                 reader.close();
         }
     }
+
 }
